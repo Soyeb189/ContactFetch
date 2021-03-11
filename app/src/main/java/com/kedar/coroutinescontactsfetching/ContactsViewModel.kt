81f99dc7e6245c4a794e468e84be8f2b0554cbca
Muktyadir <muktadir189@gmail.com>
2021-03-11 18:04:06 +0600

    First Commit

---
+++ app/src/main/java/com/kedar/coroutinescontactsfetching/ContactsViewModel.kt
@@ -0,0 +1,157 @@
+package com.kedar.coroutinescontactsfetching
+
+import android.app.Application
+import android.database.Cursor
+import android.provider.ContactsContract
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+
+class ContactsViewModel(val mApplication: Application) : AndroidViewModel(mApplication) {
+
+    private val _contactsLiveData = MutableLiveData<ArrayList<Contact>>()
+    val contactsLiveData:LiveData<ArrayList<Contact>> = _contactsLiveData
+
+    fun fetchContacts(searchString: String) {
+        viewModelScope.launch {
+            val contactsListAsync = async {
+                if(searchString.isEmpty()) {
+                    getPhoneContacts(searchString)
+                }else{
+                    getPhoneContacts2(searchString)
+                }
+            }
+            val contactNumbersAsync = async { getContactNumbers() }
+            val contactEmailAsync = async { getContactEmails() }
+
+            val contacts = contactsListAsync.await()
+            val contactNumbers = contactNumbersAsync.await()
+            val contactEmails = contactEmailAsync.await()
+
+            contacts.forEach {
+                contactNumbers[it.id]?.let { numbers ->
+                    it.numbers = numbers
+                }
+                contactEmails[it.id]?.let { emails ->
+                    it.emails = emails
+                }
+            }
+            _contactsLiveData.postValue(contacts)
+        }
+    }
+
+    private suspend fun getPhoneContacts2(searchString:String): ArrayList<Contact> {
+        val contactsList = ArrayList<Contact>()
+
+        val whereString = "display_name LIKE ?"
+        val whereParams = arrayOf("%" + searchString + "%")
+
+        val searchStringArray:Array<String>? = arrayOf(searchString)
+
+        val contactsCursor = mApplication.contentResolver?.query(
+                ContactsContract.Contacts.CONTENT_URI,
+                null,
+                whereString,
+                  //"display_name = ? ", //"display_name "+" like '%"+'?'+"%'"
+                whereParams,
+                ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " ASC")
+
+
+        if (contactsCursor != null && contactsCursor.count > 0) {
+            val idIndex = contactsCursor.getColumnIndex(ContactsContract.Contacts._ID)
+            val nameIndex = contactsCursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)
+            while (contactsCursor.moveToNext()) {
+                val id = contactsCursor.getString(idIndex)
+                val name = contactsCursor.getString(nameIndex)
+                if (name != null) {
+                    contactsList.add(Contact(id, name))
+                }
+            }
+            contactsCursor.close()
+        }
+        return contactsList
+    }
+
+    private suspend fun getPhoneContacts(searchString:String): ArrayList<Contact> {
+        val contactsList = ArrayList<Contact>()
+        val contactsCursor = mApplication.contentResolver?.query(
+                ContactsContract.Contacts.CONTENT_URI,
+                null,
+                null,
+                null,
+                ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " ASC")
+
+
+        if (contactsCursor != null && contactsCursor.count > 0) {
+            val idIndex = contactsCursor.getColumnIndex(ContactsContract.Contacts._ID)
+            val nameIndex = contactsCursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)
+            while (contactsCursor.moveToNext()) {
+                val id = contactsCursor.getString(idIndex)
+                val name = contactsCursor.getString(nameIndex)
+                if (name != null) {
+                    contactsList.add(Contact(id, name))
+                }
+            }
+            contactsCursor.close()
+        }
+        return contactsList
+    }
+
+    private suspend fun getContactNumbers(): HashMap<String, ArrayList<String>> {
+        val contactsNumberMap = HashMap<String, ArrayList<String>>()
+        val phoneCursor: Cursor? = mApplication.contentResolver.query(
+                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                null,
+                null,
+                null,
+                null
+        )
+        if (phoneCursor != null && phoneCursor.count > 0) {
+            val contactIdIndex = phoneCursor!!.getColumnIndex(ContactsContract.CommonDataKinds.Phone.CONTACT_ID)
+            val numberIndex = phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)
+            while (phoneCursor.moveToNext()) {
+                val contactId = phoneCursor.getString(contactIdIndex)
+                val number: String = phoneCursor.getString(numberIndex)
+                //check if the map contains key or not, if not then create a new array list with number
+                if (contactsNumberMap.containsKey(contactId)) {
+                    contactsNumberMap[contactId]?.add(number)
+                } else {
+                    contactsNumberMap[contactId] = arrayListOf(number)
+                }
+            }
+            //contact contains all the number of a particular contact
+            phoneCursor.close()
+        }
+        return contactsNumberMap
+    }
+
+    private suspend fun getContactEmails(): HashMap<String, ArrayList<String>> {
+        val contactsEmailMap = HashMap<String, ArrayList<String>>()
+        val emailCursor = mApplication.contentResolver.query(ContactsContract.CommonDataKinds.Email.CONTENT_URI,
+                null,
+                null,
+                null,
+                null)
+        if (emailCursor != null && emailCursor.count > 0) {
+            val contactIdIndex = emailCursor.getColumnIndex(ContactsContract.CommonDataKinds.Email.CONTACT_ID)
+            val emailIndex = emailCursor.getColumnIndex(ContactsContract.CommonDataKinds.Email.ADDRESS)
+            while (emailCursor.moveToNext()) {
+                val contactId = emailCursor.getString(contactIdIndex)
+                val email = emailCursor.getString(emailIndex)
+                //check if the map contains key or not, if not then create a new array list with email
+                if (contactsEmailMap.containsKey(contactId)) {
+                    contactsEmailMap[contactId]?.add(email)
+                } else {
+                    contactsEmailMap[contactId] = arrayListOf(email)
+                }
+            }
+            //contact contains all the emails of a particular contact
+            emailCursor.close()
+        }
+        return contactsEmailMap
+    }
+
+}
